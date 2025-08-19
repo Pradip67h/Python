da7daa06cd2dd3d49a082702d77449a39437ecdf
Pradip Hazra <codedot12@gmail.com>
2025-08-18 20:21:16 -0930

    Create variables.py
    variables in python

---
+++ 01_basic/variables.py
@@ -0,0 +1,43 @@
+# String
+name = "Pradip"
+print(type(name))
+
+# Integer
+roll_number = 17
+print(type(roll_number))
+
+# Floating number
+percentage = 79.8
+print(type(percentage))
+
+# Boolean
+is_student = True
+print(type(is_student))
+
+print(name, roll_number, percentage, is_student)
+
+percentage = 93
+print(name, roll_number, percentage, is_student)
+
+print("My name is " + name + " and my roll number is", roll_number)
+print("I scored", percentage, "% in my final exams. I am a student is", is_student)
+
+# print expressions
+print("My percentage has changed to", percentage - 1.9)
+
+# print with separator
+print(name, roll_number, percentage, is_student, sep="-")
+
+x = 1
+y = 2
+z = 3
+print(x,y,z,sep="->")
+
+# variable concatenation 
+first_name = "Pradip"
+last_name = "Hazra"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+
