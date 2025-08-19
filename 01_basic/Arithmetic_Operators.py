9df76172b519e8ea209fbf334e356dc8206c3fd2
Pradip Hazra <codedot12@gmail.com>
2025-08-18 21:16:33 -0930

    Create Arithmetic_Operators.py
    Arithmetic Operators in python

---
+++ 01_basic/Arithmetic_Operators.py
@@ -0,0 +1,20 @@
+# Addition
+print("Sum:", 9+6)
+
+# Subtraction
+print("Difference:", 9-6)
+
+# Multiplication
+print("Product:", 9*6)
+
+# Division
+print("Division:", 9/6)
+
+# Floor Division
+print("Floor Dividion:", 9//6)
+
+# Modulus
+print("Remainder:", 9%6)
+
+# Exponentiation
+print("Exponentiation:", 9**6)
