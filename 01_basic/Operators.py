3fc247dfd395889df995a4afc1b1f7a9b1f0def7
Pradip Hazra <codedot12@gmail.com>
2025-08-19 00:10:19 -0930

    Update Operators.py
    change the commit

--- 01_basic/Operators.py
+++ 01_basic/Operators.py
@@ -20,4 +20,59 @@ print("Remainder:", 9%6)
 # Exponentiation
 print("Exponentiation:", 9**6)
 
-#2. Assignment Operators
+#2. Assignmrnt Operators
+n1 = 5
+n2 = n1
+print(n1, n2)
+n2 += n1
+print(n1, n2)
+n2 -= n1
+print(n1, n2)
+n2 *= n1
+print(n1, n2)
+n2 /= n1
+print(n1, n2)
+n2 //= n1
+print(n1, n2)
+n2 %= n1
+print(n1, n2)
+
+#3. Comparison Operators
+n1 = 4
+n2 = 3
+print(n1>n2)
+print(n1<n2)
+print(n1==n2)
+print(n1!=n2)
+print(n1>=n2)
+print(n1<=n2)
+
+#4. Logical Operators
+exp1 = 2 > 1  # True 
+exp2 = 5 < 4  # False
+print("exp1 and exp2:", exp1 and exp2)
+print("exp1 or exp2:", exp1 or exp2)
+print("not exp1:", not(exp1))
+
+#5. Identity Operators
+x = 5
+y = 5
+print("if x is y:", x is y)
+print("if x is not y:", x is not y)
+
+#6. Membership Operators
+fruits = ["Apple", "Banana", "Chetty"]
+print("if Banana is present in fruits:", "Banana" in fruits)
+print("if Mango is not present in fruits:", "Mango" not in fruits)
+
+#7. Bitwise Operators
+a = 5
+b = 3
+print(" a and b:", a & b)
+print(" a or b:", a | b)
+print(" a xor b:", a ^ b)
+
+
+
+
+
