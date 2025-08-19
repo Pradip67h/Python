71d334f10da726e6de87720dbf3b36acd6e48c40
Pradip Hazra <codedot12@gmail.com>
2025-08-18 21:04:52 -0930

    Create sumOf_2_number.py
    Sum of 2 given number in python

---
+++ 01_basic/sumOf_2_number.py
@@ -0,0 +1,6 @@
+# Sum of 2 given number?
+
+num1 = int(input("Enter Your First Number: "))
+num2 = int(input("Enter Your Second Number: "))
+
+print("first number is", num1 ,"And Second number is", num2 ,"Total Number is : ",num1+num2)
