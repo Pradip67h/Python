5ce0ac4ecbc3fa35be56bc980226fecdf1219dd2
Pradip Hazra <codedot12@gmail.com>
2025-08-19 01:06:47 -0930

    Create MCQ.py
    total 4 mcq question solving in python

---
+++ 01_basic/MCQ.py
@@ -0,0 +1,20 @@
+#1. What will be the output of the following code snippet?
+a = 10
+b = 5
+print(a>b and a<15)   # (a)True
+
+#2. What will be the output of the following code snippet?
+x = 5
+y = "2"
+#print(x+y)     # (c)TypeError
+print(x+int(y))  # 7
+print(str(x)+y)  # "52"
+
+#3. What is the retult of the following expression in python?
+p = 8//3+4%2
+print(p)   # (d)2
+
+#4. What will be the value of d is a float after the operation d=2/7?
+d = 2/7
+print(d)  # (b)0.2857
+
