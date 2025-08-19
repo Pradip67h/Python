a39d56b294605bd7b037452767f28f0cd9e59c23
Pradip Hazra <codedot12@gmail.com>
2025-08-18 20:47:47 -0930

    Create dataType.py
    data type in python

---
+++ 01_basic/dataType.py
@@ -0,0 +1,52 @@
+char = "A"
+print(ord(char))
+
+char = "B"
+print(ord(char))
+
+char = "Z"
+print(ord(char))
+
+
+char = "a"
+print(ord(char))
+
+char = "z"
+print(ord(char))
+
+
+ascii = 97
+print(chr(ascii))  
+
+ascii = 33
+print(chr(ascii))
+
+ascii = 64
+print(chr(ascii))
+
+ascii = 36
+print(chr(ascii))
+
+ascii = 37
+print(chr(ascii))
+
+ascii = 38
+print(chr(ascii))
+
+ascii = 42
+print(chr(ascii))
+
+student = {
+    "first_name": "Pradip",
+    "last_name": "Hazra",
+    "rollno": 876543,
+    "city": "Haldia"
+}
+
+print(student)
+
+
+thisset = {"Apple", "Banana", "Cherry", "Mango"}
+print(thisset)
+
+
