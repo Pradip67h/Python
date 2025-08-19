ec3c0f45c1949a4b2f65a5584c401142a53627d7
Pradip Hazra <codedot12@gmail.com>
2025-08-19 00:20:05 -0930

    Create Volume_of_sphere.py
    calculating volume of sphere in python

---
+++ 01_basic/Volume_of_sphere.py
@@ -0,0 +1,10 @@
+# Calculating Volume of a sphere?
+
+#taking radius as input
+radius = float(input("Enter radius of sphere: "))
+
+#calculate volume
+volume = 4 / 3 * 3.14 * (radius ** 3)
+
+#print volume of sphere
+print("Volume of sphere is:", volume)
