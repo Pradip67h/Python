52fe229c0a285a25f36b02dc23383c3dc6ed1d20
Pradip Hazra <codedot12@gmail.com>
2025-08-19 19:54:57 -0930

    Create if_else.py
    Take an integer input and tell if it is positive or negative (zero not included) in python

---
+++ 01_basic/if_else.py
@@ -0,0 +1,7 @@
+# Take an integer input and tell if it is positive or negative (zero not included)?
+
+number = int(input("Enter a Number: "))
+if number >= 0:
+    print("Number is Positive integer!")
+else:
+    print("Number is Negative Integer!")
