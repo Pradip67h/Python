28a7aadf1bffa8e65d6f472026eeca3c7b5d90fa
Pradip Hazra <codedot12@gmail.com>
2025-08-19 19:56:39 -0930

    Create even_odd.py
     Take a positive integer input and tell if it is even or odd in python

---
+++ 01_basic/even_odd.py
@@ -0,0 +1,7 @@
+#  Take a positive integer input and tell if it is even or odd?
+
+number = int(input("Enter a Positive Number : "))
+if number % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")    
