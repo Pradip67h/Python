e86f7b5ff3657c7dc3c555fdeba711282f4fbfce
Pradip Hazra <codedot12@gmail.com>
2025-08-21 01:12:03 -0930

    Create Print_element_for_loop.py
    print element using for loop in python

---
+++ 01_basic/Print_element_for_loop.py
@@ -0,0 +1,9 @@
+# Print element of a list using for loop ?
+
+list = [1, 2, 3, 4, 5, 6]
+fruits = ["Apple", "Banana", "Mango", "Kiwi"]
+
+for i in list:
+    print(i)
+for j in fruits:    
+    print(j)
