7dca85a7ed903fc8dc639fae9e7cf7b28b1d3b5b
Pradip Hazra <codedot12@gmail.com>
2025-08-21 01:33:04 -0930

    Create display_AP.py
    display this AP 1 to n in python

---
+++ 01_basic/display_AP.py
@@ -0,0 +1,13 @@
+# Display this AP - 1,3,5,7,9.. upto 'n' terms ?
+
+n = int(input("Enter the number of terms: "))
+ 
+current_term = 1
+common_difference = 2
+end_term = current_term + (n-1)*common_difference
+print("\nAP:", end = " ")
+for _ in range(current_term, end_term, common_difference):
+    print(current_term, end=", ")
+    current_term += common_difference
+print(current_term)
+ 
