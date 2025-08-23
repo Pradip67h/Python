e40a7c36a8a142bf0a2528ed97ee66cc58aa0495
Pradip Hazra <codedot12@gmail.com>
2025-08-23 01:35:44 -0930

    Create maximum_minimum.py
    maximum and minimum element in a set in python

---
+++ 01_basic/maximum_minimum.py
@@ -0,0 +1,11 @@
+# Print the maximum and minimum element in a set in Python?
+
+def MIN(sets):
+    return (min(sets))
+def MAX(sets):
+    return (max(sets))
+
+# Driver Code
+sets = set([4, 12, 10, 9, 4, 13, 8, 16, 24, 1, 25, 3, 10, 65, 55])
+print(MIN(sets))
+print(MAX(sets))
