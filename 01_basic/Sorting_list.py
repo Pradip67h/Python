5db9db0d15c6ff89fc2198156ae091afe21841cb
Pradip Hazra <codedot12@gmail.com>
2025-08-22 20:14:11 -0930

    Create Sorting_list.py
    sorting list in python

---
+++ 01_basic/Sorting_list.py
@@ -0,0 +1,13 @@
+# Sorting List
+fruits = ["apple", "cherry", "banana", "papaya", "mango", "kiwi"]
+print(fruits)
+
+fruits.sort()   # by default ascending
+print(fruits)
+
+fruits.sort(reverse=True)  # descending
+print(fruits)
+
+# Reversing a list
+fruits.reverse()
+print(fruits)
