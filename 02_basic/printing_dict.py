106a95f0c294166d47e847aeab972c3a5fa5b962
Pradip Hazra <codedot12@gmail.com>
2025-08-24 06:26:33 -0930

    Create printing_dict.py
    printing dictionarirs in pyton

---
+++ 02_basic/printing_dict.py
@@ -0,0 +1,13 @@
+# Creating a Dictionary
+phones = {
+    "John" : 98763,
+    "Atanu" : 68922,
+    "Ayan" : 98763,
+    "Suman" : 88672
+}
+print(phones)
+
+# printing elements of a dict
+for x,y in phones.items():
+    print(x,y)
+    
