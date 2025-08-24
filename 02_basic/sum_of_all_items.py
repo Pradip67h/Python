563465490b255b2377c4f8d8be91037eef712be8
Pradip Hazra <codedot12@gmail.com>
2025-08-24 06:38:04 -0930

    Create sum_of_all_items.py
    sum of all items dictionary in python

---
+++ 02_basic/sum_of_all_items.py
@@ -0,0 +1,9 @@
+# Givan a dictionary in Python, write a python program to find the sum of all items in the dictionary ?
+
+dict = {
+    'a' : 100,
+    'b' : 200,
+    'c' : 300
+}
+
+print("The sum of dict values is: ",sum(dict.values()))
