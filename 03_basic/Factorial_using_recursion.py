583653130fbf51127e5cdce94fddbedaa2277927
Pradip Hazra <codedot12@gmail.com>
2025-08-27 00:50:06 -0930

    Create Factorial_using_recursion.py
    factorial print using recursion in python

---
+++ 03_basic/Factorial_using_recursion.py
@@ -0,0 +1,14 @@
+# Factorial print using Recursion 
+
+def factorial(n):
+    
+    # base case
+    if n == 0:
+        return 1
+    
+    # recursive case
+    ans = n * factorial(n-1)
+    return ans
+
+n = int(input("Enter n: "))
+print("the Factorial is:",factorial(n))
