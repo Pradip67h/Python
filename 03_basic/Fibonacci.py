e102faacdb67f0ef031f38b013362f4e3827f941
Pradip Hazra <codedot12@gmail.com>
2025-08-27 01:35:15 -0930

    Create Fibonacci.py
    fibonacci series using recursion in python

---
+++ 03_basic/Fibonacci.py
@@ -0,0 +1,16 @@
+# Program on Fibonacci Series.
+
+def fibonacci(n):
+    
+    # base case
+    if n == 1:   # base case
+        return 0
+    elif n == 2:   # base case
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)   # recursive case
+    
+n = int(input("Enter n: "))
+for i in range(1, n+1):
+    print("Print Fibonacci Series",fibonacci(i))    
+    
