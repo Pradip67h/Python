4abb061d87106915b7584a2796f0af8d7f616382
Pradip Hazra <codedot12@gmail.com>
2025-08-28 04:47:54 -0930

    Create Slicing.py
    Slicing in python

---
+++ 04_basic/Slicing.py
@@ -0,0 +1,11 @@
+# Slicing a string
+
+text = "Hello, World!"
+ 
+# Slice from the beginning to index 5 (exclusive)
+slice1 = text[:5]
+print(slice1)  # Output: Hello
+ 
+# Slice from the beginning to index 7 (exclusive)
+slice2 = text[:7]
+print(slice2)  # Output: Hello,
