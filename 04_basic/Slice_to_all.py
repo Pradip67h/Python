49ac3551b139a4d6946b671abac20ef50fcffb28
Pradip Hazra <codedot12@gmail.com>
2025-08-28 04:54:28 -0930

    Create Slice_to_all.py
    Slice to the all in python

---
+++ 04_basic/Slice_to_all.py
@@ -0,0 +1,45 @@
+# Slice to all..
+
+text = "Hello, World!"
+ 
+# Slice from index 0 to index 5 (exclusive)
+slice1 = text[0:5]
+print(slice1)  # Output: Hello
+ 
+# Slice from index 7 to the end of the string
+slice2 = text[7:]
+print(slice2)  # Output: World!
+ 
+# Slice from the beginning to index 5 (exclusive)
+slice3 = text[:5]
+print(slice3)  # Output: Hello
+ 
+# Slice the last 6 characters of the string
+slice4 = text[-6:]
+print(slice4)  # Output: World!
+ 
+# Slice from index 0 to index 11 (exclusive) with a step of 2
+slice5 = text[0:11:2]
+print(slice5)  # Output: text = "Hello, World!"
+ 
+# Slice from index 0 to index 5 (exclusive)
+slice1 = text[0:5]
+print(slice1)  # Output: Hello
+ 
+# Slice from index 7 to the end of the string
+slice2 = text[7:]
+print(slice2)  # Output: World!
+ 
+# Slice from the beginning to index 5 (exclusive)
+slice3 = text[:5]
+print(slice3)  # Output: Hello
+ 
+# Slice the last 6 characters of the string
+slice4 = text[-6:]
+print(slice4)  # Output: World!
+ 
+# Slice from index 0 to index 11 (exclusive) with a step of 2
+slice5 = text[0:11:2]
+print(slice5)  # Output: Hlo ol
+ 
+ 
