3282fee05e90850696cbbbc500a64fe782f69cb6
Pradip Hazra <codedot12@gmail.com>
2025-08-28 04:57:30 -0930

    Create Slicing_negative_indx.py
    Slicing negative indexing in python

---
+++ 04_basic/Slicing_negative_indx.py
@@ -0,0 +1,16 @@
+# Slicing a string in negative indexing
+
+text = "Hello, World!"
+ 
+# Access the last character using -1
+last_char = text[-1]
+print(last_char)  # Output: !
+ 
+# Access the second-to-last character using -2
+second_last_char = text[-2]
+print(second_last_char)  # Output: d
+ 
+# Slicing using negative indexing
+slice1 = text[-6:-1]
+print(slice1)  # Output: World
+ 
