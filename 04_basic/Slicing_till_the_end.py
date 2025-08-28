668622a7083c249329e49693867292a72265f0a8
Pradip Hazra <codedot12@gmail.com>
2025-08-28 04:50:51 -0930

    Create Slicing_till_the_end.py
    Slicing till the end in python

---
+++ 04_basic/Slicing_till_the_end.py
@@ -0,0 +1,11 @@
+# Slicing till the end
+
+text = "Hello, World!"
+ 
+# Slice from index 7 to the end of the string
+slice1 = text[7:]
+print(slice1)  # Output: World!
+ 
+# Slice from index 2 to the end of the string
+slice2 = text[2:]
+print(slice2)  # Output: llo, World!
