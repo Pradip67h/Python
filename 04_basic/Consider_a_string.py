360db359089a00a5a59cf020c5a3dc3a1793621e
Pradip Hazra <codedot12@gmail.com>
2025-08-29 20:48:54 -0930

    Create Consider_a_string.py
    consider a string using all type of string in python

---
+++ 04_basic/Consider_a_string.py
@@ -0,0 +1,27 @@
+# Consider a string : 'The unexpected always happens'.
+
+
+# 1. Assign this string to a variable: text.
+text = "the unexpected always happens"
+
+# 2. Print the string.
+print(text)
+
+# 3. Print the length of the string.
+print(len(text))
+
+# 4. Check if the phrase 'pp' is present in the string.
+print(text.find('pp'))
+
+# 5. Print the substring from 0 till 10th index.
+print(text[:11])
+
+# 6. Replace 'always' with 'never'.
+print(text.replace('always', 'never'))
+
+# 7. Add "no matter what" to the string.
+text2 = " no matter what"
+final_text = text + text2
+
+# 8. Print the final string.
+print(final_text)
